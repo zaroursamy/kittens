3935f3e880d5d394a3b89d3741af96c7fb9f3184
zaroursamy <samy.zarour8430@gmail.com>
2016-10-13 19:29:35 +0200

    add multiple tests

--- src/test/scala/LogicSpec.scala
+++ src/test/scala/LogicSpec.scala
@@ -12,4 +12,22 @@ object LogicSpec extends Specification {
       result must beGreaterThan(.999)
     }
   }
+
+  "The 'matchLikelihood' method" should {
+    "be 0% when no attributes match" in {
+      val tabby = Kitten(1, List("male", "tabby"))
+      val prefs = Preference(List("female", "calico"))
+      val result = Logic.matchLikelihood(tabby, prefs)
+      result must beLessThan(.001)
+    }
+  }
+
+  "The 'matchLikelihood' method" should {
+    "be 50% when the half of attributes match" in {
+      val tabby = Kitten(1, List("male","tabby"))
+      val prefs = Preference(List("male", "notabby"))
+      val result = Logic.matchLikelihood(tabby, prefs)
+      result must beEqualTo(0.5)
+    }
+  }
 }
